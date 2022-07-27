8784a208976ceb6e60c663d261e9a0377e59a3db
Maxim <metallmuff@gmail.com>
2022-07-27 13:17:00 +1000

    Конец ДЗ

--- Program.cs
+++ Program.cs
@@ -25,7 +25,8 @@ int InputNumbers(string input)
 */
 /////////////// Конец первой задачи /////////////////
 
-// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
@@ -64,4 +65,35 @@ int InputNumbers(string input)
 */
 //////////////////////////  Конец второй задачи /////////////////
 
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+
+/*
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана ровна: {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+*/
+
+
+/////////////////////////// конец ДЗ /////////////////////////
 
